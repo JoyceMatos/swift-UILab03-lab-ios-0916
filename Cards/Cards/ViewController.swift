e5430b99be792da66b145ecafe716ea34393a2d9
JoyceMatos <j.matosjoyce@gmail.com>
2016-09-08 09:42:30 -0400

    Completed

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,11 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // TODO: Create IB outlets
 
+    @IBOutlet weak var topLeft: UILabel!
+    
+    @IBOutlet weak var center: UILabel!
+    
+    @IBOutlet weak var bottomRight: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func clubs(sender: AnyObject) {
+        print("Clubs Clicked")
+        topLeft.text = String("♣️")
+        center.text = String("4")
+        bottomRight.text = String("♣️")
+        }
+    
+    @IBAction func spades(sender: AnyObject) {
+        print("Spades Clicked")
+        topLeft.text = String("♠️")
+        center.text = String("3")
+        bottomRight.text = String("♠️")
+    }
+    
+    @IBAction func diamonds(sender: AnyObject) {
+        print("Diamonds Clicked")
+        topLeft.text = String("♦️")
+        center.text = String("8")
+        bottomRight.text = String("♦️")
+    }
+    
+    @IBAction func hearts(sender: AnyObject) {
+        print("Hearts Clicked")
+        topLeft.text = String("♥️")
+        center.text = String("10")
+        bottomRight.text = String("♥️")
+    }
+    
+    
+    
     // TODO: IB actions and code to update UI
 }
